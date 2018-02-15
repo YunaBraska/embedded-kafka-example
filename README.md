c34d52e6b13fa6af1ce0e1ee5ce7e26e19be5583
yuna90 <yuna-@web.de>
2018-02-15 19:40:31 +0100

    fixed dependency version problems

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 This is a very simple spring boot example of how to test sender and receiver with an embedded kafka server
 
 #### Things to keep in mind
- 
+
+* Upgrading kafka wont be a good idea since there are coming strange dependency problems 
 * Please do not use the test as a "best practice" example ;)
 * Feel free and make merge requests for perfection :)
--- src/test/java/com/springfrosch/kafkaexample/kafkaexample/controller/ControllerTest.java
+++ src/test/java/com/springfrosch/kafkaexample/kafkaexample/controller/ControllerTest.java
@@ -4,12 +4,15 @@ import com.springfrosch.kafkaexample.kafkaexample.util.SpringBootEmbeddedKafka;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+//You could take @EmbeddedKafka annotation but it wont be included in newer versions of "spring-kafka-test"
+//@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class ControllerTest extends SpringBootEmbeddedKafka {
 
     @Autowired
